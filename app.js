27109ae4c81b8421e21034601c5a2fcd483d4541
Hemanth Kumar V S <hemanth.kumarvs.intern@lightmetrics.co>
2023-06-29 14:25:11 +0530

    Add end point to get hello message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello')
 })
 app.get('/health', (req, res) => {
   res.send('Health Checked!');
