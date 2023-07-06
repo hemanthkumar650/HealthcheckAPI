89ee39ccbbef9bd0074d9a2556a83fc1a3864baa
Hemanth Kumar V S <hemanth.kumarvs.intern@lightmetrics.co>
2023-07-06 11:10:18 +0530

    Conflict Resolved

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const base64url = require('base64url');
-
-<<<<<<< HEAD
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   const response = { message: 'Hello' };
@@ -9,16 +9,15 @@ app.get('/', (req, res) => {
 });
 app.get('/health', (req, res) => {
   res.send('Health Checked!');
-=======
-const app = express();
-const port = 3000;
+});
+
+
 
 app.get('/text-base64', (req, res) => {
   const { text } = req.query;
   const base64 = Buffer.from(text).toString('base64');
 
   res.json({ text, base64 });
->>>>>>> base64
 });
 
 app.listen(port, () => {
